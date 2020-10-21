060683f7e6268bd19683e45f94962088cdeb3f50
yangqian <743295483@qq.com>
2020-10-21 17:59:02 +0800

    [yangqian] feat: user service including get user info and user educations.

---
+++ src/_services/users.service.js
@@ -0,0 +1,31 @@
+import { handleResponse } from "../_helpers/handle-response";
+
+const apiUrl = "http://127.0.0.1:8080";
+
+export function getUserInfo(userId) {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Accept: "application/json;charset=UTF-8",
+      "Content-Type": "application/json",
+      Cache: "no-cache",
+    },
+  };
+  return fetch(`${apiUrl}/users/${userId}`, requestOptions).then(
+    handleResponse
+  );
+}
+
+export function getUserEducations(userId) {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      Accept: "application/json;charset=UTF-8",
+      "Content-Type": "application/json",
+      Cache: "no-cache",
+    },
+  };
+  return fetch(`${apiUrl}/users/${userId}/educations`, requestOptions).then(
+    handleResponse
+  );
+}
